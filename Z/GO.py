e28ad2251733dd6bbf815f9769005440979de0d7
sudoreboot2020 <55040629+kotik06@users.noreply.github.com>
2020-06-15 11:57:17 +0700

    Update GO.py

--- Z/GO.py
+++ Z/GO.py
@@ -13,7 +13,6 @@ print(Fore.GREEN +
 print(Fore.GREEN+'Enter Lang :')
 print("\033[31m[\033[33m1\033[31m] - RUS/РУС")
 print("\033[31m[\033[33m2\033[31m] - ENG/АНГ")
-os.chdir('Z')
 while True:
     lag = input("\033[35m\033[5m[*]")
     if lag == ('1'):
